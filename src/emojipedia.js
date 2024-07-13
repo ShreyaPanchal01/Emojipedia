a803015c939f4d1c23b6c408673502bb6affff7a
ShreyaPanchal01 <155734052+ShreyaPanchal01@users.noreply.github.com>
2024-07-13 21:49:32 +0530

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,75 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+
+  {
+    id: 4,
+    emoji: "😅",
+    name: "Smiling Face with Open Mouth & Cold Sweat",
+    meaning:
+      "A smiley face with a bead of sweat, showing a combination of happiness and nervousness. Can be used to convey a close call or a minor relief.",
+  },
+  {
+    id: 5,
+    emoji: "😍",
+    name: "Smiling Face with Heart-Eyes",
+    meaning:
+      "A smiley face with heart-shaped eyes, expressing love, adoration, or strong liking. Often used to convey feelings of love and admiration.",
+  },
+  {
+    id: 6,
+    emoji: "😢",
+    name: "Crying Face",
+    meaning:
+      "A face with a tear running down its cheek. Represents sadness, grief, or pain. Can also be used to show sympathy or regret.",
+  },
+  {
+    id: 7,
+    emoji: "😎",
+    name: "Smiling Face with Sunglasses",
+    meaning:
+      "A cool smiley wearing sunglasses, often used to convey a sense of confidence, self-assuredness, or just plain coolness.",
+  },
+  {
+    id: 8,
+    emoji: "🤔",
+    name: "Thinking Face",
+    meaning:
+      "A face with a hand on its chin, showing a pondering or thinking expression. Often used to indicate thinking, pondering, or consideration.",
+  },
+  {
+    id: 9,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "A smiley face turned upside down, often used to convey sarcasm, irony, or joking. Can also represent a playful or silly mood.",
+  },
+  {
+    id: 10,
+    emoji: "😇",
+    name: "Smiling Face with Halo",
+    meaning:
+      "A smiling face with a halo above its head, often used to indicate innocence, goodness, or a good deed.",
+  },
+];
+
+export default emojipedia;
